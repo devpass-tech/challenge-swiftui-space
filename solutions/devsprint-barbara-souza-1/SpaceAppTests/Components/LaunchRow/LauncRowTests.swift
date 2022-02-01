cbed0674693c657d24035c6aa4398ad0953d2891
Barbara Souza <barbara.souza@tui.co.uk>
2022-02-01 11:11:26 +0000

    Fix tests

--- solutions/devsprint-barbara-souza-1/SpaceAppTests/Components/LaunchRow/LauncRowTests.swift
+++ solutions/devsprint-barbara-souza-1/SpaceAppTests/Components/LaunchRow/LauncRowTests.swift
@@ -12,7 +12,11 @@ import XCTest
 class LauncRowTests: XCTestCase {
 
     func testLaunchRowAppearance() {
-        let sut = LaunchRowView()
+        let sut = LaunchRowView(viewModel: LaunchRowViewModel(launchPatchURL:  "https://images2.imgbox.com/02/51/7NLaBm8c_o.png",
+                                                              launchName: "CRS-20",
+                                                              launchDate: "July 03, 2020",
+                                                              launchStatus: "Success",
+                                                              launchNumber: "#91"))
             .frame(width: 414, height: 612)
         
         assertSnapshot(matching: sut, as: .image)
