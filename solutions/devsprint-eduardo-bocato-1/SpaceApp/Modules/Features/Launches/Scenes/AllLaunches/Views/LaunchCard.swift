be25699a1fcc508124571cbb007859a779abb3ac
RazeeHJ <razee.hj@gmail.com>
2022-03-15 13:53:37 +0300

    Implements LaunchCard Model

---
+++ solutions/devsprint-eduardo-bocato-1/SpaceApp/Modules/Features/Launches/Scenes/AllLaunches/Views/LaunchCard.swift
@@ -0,0 +1,31 @@
+//
+//  LaunchCard.swift
+//  SpaceApp
+//
+//  Created by Razee Hussein-Jamal on 15/03/2022.
+//
+
+import SwiftUI
+
+struct LaunchCard: View {
+    struct Model {
+        let imageURL: String
+        let date: String
+        let name: String
+        let number: Int
+        let state: Bool
+    }
+    
+    let model: Model
+    
+    var body: some View {
+        
+        
+    }
+}
+
+struct LaunchCard_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchCard()
+    }
+}
