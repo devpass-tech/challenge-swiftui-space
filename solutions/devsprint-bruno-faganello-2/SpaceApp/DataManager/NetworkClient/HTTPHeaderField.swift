4c1fc08cb94c2716422443afe982ce09d381ad28
Rodrigo Borges <rborges.soares@gmail.com>
2022-04-18 14:34:50 -0300

    Projeto base para Dev Sprint Bruno Faganello #2

---
+++ solutions/devsprint-bruno-faganello-2/SpaceApp/DataManager/NetworkClient/HTTPHeaderField.swift
@@ -0,0 +1,12 @@
+//
+//  HTTPHeaderField.swift
+//  SpaceApp
+//
+//  Created by Rodrigo Borges on 04/01/22.
+//
+
+public enum HTTPHeaderField: String {
+
+    case contentType = "Content-Type"
+    case authorization = "Authorization"
+}
