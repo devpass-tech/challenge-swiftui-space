b8e7260d7853a22fdc8904305dc08e59ef29896f
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-04-25 20:08:05 -0300

    Feature (LaunchView): Create a file

---
+++ solutions/devsprint-bruno-faganello-2/SpaceApp/Screens/LaunchView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchView.swift
+//  SpaceApp
+//
+//  Created by Paolo Prodossimo Lopes on 25/04/22.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text("Launch View")
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
