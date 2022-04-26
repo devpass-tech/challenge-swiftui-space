6c800efd035b912b9a6901a849afcd0cc92b4bc5
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-04-26 20:31:01 -0300

    Feature (Endpoints): Create a protocol and Classes
    Description:
    - Create a Endpoint Protocol to use for request
    - Create a AllLaunchsEndpoint to get all launches
    - Create a NextLaunchsEndpoint to get next launch

--- solutions/devsprint-bruno-faganello-2/SpaceApp/DataManager/Endpoint/Endpoint.swift
+++ solutions/devsprint-bruno-faganello-2/SpaceApp/DataManager/Endpoint/Endpoint.swift
@@ -5,4 +5,14 @@
 //  Created by Paolo Prodossimo Lopes on 26/04/22.
 //
 
-import Foundation
+protocol Endpoint {
+    var baseURL: String { get }
+    var path: String { get }
+    var parameters: [String : String] { get }
+    var httpHeaders: [HTTPHeaderField : String] { get }
+}
+
+extension Endpoint {
+    var baseURL: String { "https://api.spacexdata.com/v5/launches" }
+    var url: URL { URL(string: "\(baseURL + path)")! }
+}
